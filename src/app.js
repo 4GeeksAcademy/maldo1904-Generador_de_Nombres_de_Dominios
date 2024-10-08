0e529b5c23e90ffb9bcbfc4ed0fce74c8d88ce37
José Daniel Maldonado Flores <danielmaldo_31@hotmail.com>
2024-10-08 03:19:41 +0000

    termine

--- src/app.js
+++ src/app.js
@@ -5,7 +5,18 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+//write your code here
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let fin = [".com", ".net", ".org"];
+
+for (let index = 0; index < pronoun.length; index++) {
+  for (let i = 0; i < adj.length; i++) {
+    for (let ind = 0; ind < noun.length; ind++) {
+      for (let inde = 0; inde < fin.length; inde++) {
+        console.log(`${pronoun[index]}${adj[i]}${noun[ind]}${fin[inde]}`);
+      }
+    }
+  }
+}
